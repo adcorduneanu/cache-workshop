93de17e6ffd8ee05fe8f9d4268f80a7a3cd2a532
Adrian Corduneanu <adrian.corduneanu@centric.eu>
2023-01-18 17:10:17 +0200

    add server and response output

---
+++ response-cache/response-cache-controllers/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddResponseCaching();
+builder.Services.AddControllers(options =>
+{
+    options.CacheProfiles.Add("Default30",
+        new CacheProfile()
+        {
+            Duration = 30
+        });
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseResponseCaching();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

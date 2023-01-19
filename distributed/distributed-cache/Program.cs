6dadad4b0e87bb5a28f35ec379a44b1981d2a22b
Adrian Corduneanu <adrian.corduneanu@centric.eu>
2023-01-19 14:40:58 +0200

    Fix the distributed cache implementation

--- distributed/distributed-cache/Program.cs
+++ distributed/distributed-cache/Program.cs
@@ -5,6 +5,10 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetSection("Redis:ConnectionString").Get<string>();
     options.InstanceName = "cache-ws";
 });
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
